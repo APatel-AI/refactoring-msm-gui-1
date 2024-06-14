c8faaa6f74ab72da38fb3d42ae01fce1a08edba1
Arpan Patel <arpan.p.se@gmail.com>
2024-06-14 22:53:51 +0000

    added methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,28 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  def director
+    d_id = self.director_id
+
+    matching_directors = Director.where({ :id => d_id })
+
+    the_director = matching_directors.at(0)
+
+    return the_director
+  end
+
+  def characters
+    # Step 1: get the id of the record in question from the Movie table (1-n showing parent to child)
+    my_id = self.id
+
+    # Step 2: pull the corresponding record using the Movie id from the Character table
+    matching_characters = Character.where({ :movie_id => my_id })
+
+    # Step 3: return the variable as a result
+    return matching_characters
+
+    # Step 4: create the same relationship in the Character table
+  end
+
 end
